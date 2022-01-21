a6ccbf81bb9d768feeeda89cdfc013002dba5e9a
jmsrc92 <jmsrc92@gmail.com>
2022-01-21 14:00:27 +0000

    upload

--- Day-14/coffee_machine.py
+++ Day-14/coffee_machine.py
@@ -14,20 +14,17 @@ from coffee_menu import resources
 # """
 coffee_machine = "on"
 
+#  while coffee_machine == "on":
+
 drink = input("What would you like (espresso/latte/cappuccino): ")
 
-print("Please insert coins.")
-
-no_quarters = input("How many quarters?")
-no_dimes = input("How many dimes?")
-no_nickles = input("How many nickles?")
-no_pennies = input("How many pennies?")
 
 def cash_total():
     quarter_val = 0.25
     dime_val = 0.10
     nickle_val = 0.05
     penny_val = 0.01
+
 
 def print_report(money=0):
     water = resources["water"]
@@ -38,13 +35,28 @@ def print_report(money=0):
     print(f"Coffee: {coffee}")
     print(f"Money: ${money}")
 
-#  while coffee_machine == "on":
 
 if drink == "off":
     coffee_machine = "off"
 elif drink == "report":
     print_report()
 
+print("Please insert coins.")
+
+# no_quarters = input("How many quarters? ")
+# no_dimes = input("How many dimes? ")
+# no_nickles = input("How many nickles? ")
+# no_pennies = input("How many pennies? ")
+
+def check_resources(chosen_drink):
+    """Checks coffee ingredient values against machine level values returns nothing"""
+    coffee_ingredients = MENU[chosen_drink]["ingredients"]
+    for item in coffee_ingredients:
+        coffee_ingredient_value = MENU[chosen_drink]["ingredients"][item]
+        resource_ingredient_value = resources[item]
+        if resource_ingredient_value < coffee_ingredient_value:
+            print(f"Sorry there is not enough {item}")
+        resource_ingredient_value -= coffee_ingredient_value
 
 
-
+check_resources(drink)
