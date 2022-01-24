38b9df6c6cbc70388f7028d0452dc9f440c41987
jmsrc92 <jmsrc92@gmail.com>
2022-01-24 11:46:36 +0000

    upload

--- Day-14/coffee_machine.py
+++ Day-14/coffee_machine.py
@@ -13,17 +13,22 @@ from coffee_menu import resources
 # adjust report
 # """
 coffee_machine = "on"
-
+coffee_machine_cash = 0
 #  while coffee_machine == "on":
 
 drink = input("What would you like (espresso/latte/cappuccino): ")
 
 
-def cash_total():
+def cash_total(total_quarters=0, total_dimes=0, total_nickles=0, total_pennies=0):
     quarter_val = 0.25
     dime_val = 0.10
     nickle_val = 0.05
     penny_val = 0.01
+    total_quarters *= quarter_val
+    total_dimes *= dime_val
+    total_nickles *= nickle_val
+    total_pennies *= penny_val
+    return total_quarters + total_dimes + total_nickles + total_pennies
 
 
 def print_report(money=0):
@@ -43,10 +48,13 @@ elif drink == "report":
 
 print("Please insert coins.")
 
-# no_quarters = input("How many quarters? ")
-# no_dimes = input("How many dimes? ")
-# no_nickles = input("How many nickles? ")
-# no_pennies = input("How many pennies? ")
+num_quarters = int(input("How many quarters? "))
+num_dimes = int(input("How many dimes? "))
+num_nickles = int(input("How many nickles? "))
+num_pennies = int(input("How many pennies? "))
+
+total_user_money = cash_total()
+
 
 def check_resources(chosen_drink):
     """Checks coffee ingredient values against machine level values returns nothing"""
@@ -59,4 +67,10 @@ def check_resources(chosen_drink):
         resource_ingredient_value -= coffee_ingredient_value
 
 
-check_resources(drink)
+def check_transaction(user_money, chosen_drink, coffee_cash_total):
+    if user_money < chosen_drink:
+        print("Sorry that's not enough money. Money refunded.")
+    else:
+        coffee_cash_total -= user_money
+        print(f"Here is {coffee_cash_total} dollars in change.”")
+    return coffee_cash_total
