053361a4e307dda580d23f96c758396c28d76131
jmsrc92 <jmsrc92@gmail.com>
2022-01-24 17:58:55 +0000

    upload

--- Day-14/coffee_machine.py
+++ Day-14/coffee_machine.py
@@ -2,12 +2,12 @@ from coffee_menu import MENU
 from coffee_menu import resources
 
 
-coffee_machine = "on"
+coffee_machine_on = True
 coffee_machine_cash = 0
 
-  while coffee_machine == "on":
+while coffee_machine_on:
 
-    drink = input("What would you like (espresso/latte/cappuccino): ")
+
 
 
     def cash_total(total_quarters=0, total_dimes=0, total_nickles=0, total_pennies=0):
@@ -32,10 +32,13 @@ coffee_machine_cash = 0
         print(f"Money: ${coffee_machine_cash}")
 
 
+    drink = input("What would you like (espresso/latte/cappuccino): ")
+
     if drink == "off":
-        coffee_machine = "off"
+        coffee_machine_on = False
     elif drink == "report":
         print_report()
+        drink = input("What would you like (espresso/latte/cappuccino): ")
 
     print("Please insert coins.")
 
@@ -49,13 +52,16 @@ coffee_machine_cash = 0
 
     def check_resources(chosen_drink):
         """Checks coffee ingredient values against machine level values returns nothing"""
+        global coffee_machine_on
         coffee_ingredients = MENU[chosen_drink]["ingredients"]
         for item in coffee_ingredients:
             coffee_ingredient_value = MENU[chosen_drink]["ingredients"][item]
             resource_ingredient_value = resources[item]
             if resource_ingredient_value < coffee_ingredient_value:
                 print(f"Sorry there is not enough {item}")
-            resource_ingredient_value -= coffee_ingredient_value
+                coffee_machine_on = False
+            else:
+                resources[item] = resource_ingredient_value - coffee_ingredient_value #update dictionary value
 
 
     def check_transaction(user_money, chosen_drink, coffee_cash_total):
@@ -65,8 +71,8 @@ coffee_machine_cash = 0
         else:
             change = round(user_money - drink_cost, 2)
             coffee_cash_total += drink_cost
-            print(f"Here is ${change} dollars in change.”")
+            print(f"Here is ${change} dollars in change.")
         return coffee_cash_total
 
-
+    check_resources(drink)
     coffee_machine_cash = check_transaction(total_user_money, drink, coffee_machine_cash)
