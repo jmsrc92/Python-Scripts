17ed8561afa4f7d5d05b4f4991dc752a2382cb4a
jmsrc92 <jmsrc92@gmail.com>
2022-01-28 09:43:50 +0000

    final

--- Day-14/coffee_machine_v2.py
+++ Day-14/coffee_machine_v2.py
@@ -32,27 +32,19 @@ resources = {
 
 # Program start
 
-machine_on = True
+
 machine_money = 0
 
-def print_report():
-    global machine_money
-    water = resources["water"]
-    milk = resources["milk"]
-    coffee = resources["coffee"]
-    print(f"Water: {water}")
-    print(f"Milk: {milk}")
-    print(f"Coffee: {coffee}")
-    print(f"Money: ${machine_money}")
-    return
 
 def check_resources(chosen_drink):
     """Loops through coffee ingredients returns false if not enough resources
     returns True if levels are enough to make a drink"""
+    if chosen_drink == "report": return
     for item in MENU[chosen_drink]["ingredients"]: # Every ingredient within chosen drink
         if MENU[chosen_drink]["ingredients"][item] >= resources[item]:
             print(f"There is not enough {item} .")
-            return False
+            machine_on = False
+            return 
         else: return True
 
 
@@ -72,34 +64,34 @@ def make_coffee(chosen_drink):
         return
 
 
-while machine_on:
+machine_on = True
+
+while machine_on == True: # while loops stay true until they come back to the start again, if you want to break use 'break'
 
     choice = input("What would you like (espresso/latte/cappuccino): ")
 
     if choice.lower() == "off":
         machine_on = False
     elif choice.lower() == "report":
-        print_report()
-        machine_on = False
+        print(f"Water: {resources['water']}")
+        print(f"Milk: {resources['milk']}")
+        print(f"Coffee: {resources['coffee']}")
+        print(f"Money: ${machine_money}")
     elif choice not in MENU:
         print("Invalid choice")
     else:
-        check_resources(choice)
-    
-    if check_resources(choice) == False:
-        machine_on = False
+        if check_resources(choice) == True:
+            num_quarters = int(input("How many quarters? "))
+            num_dimes = int(input("How many dimes? "))
+            num_nickles = int(input("How many nickles? "))
+            num_pennies = int(input("How many pennies? "))
 
-    num_quarters = int(input("How many quarters? "))
-    num_dimes = int(input("How many dimes? "))
-    num_nickles = int(input("How many nickles? "))
-    num_pennies = int(input("How many pennies? "))
+            change = process_coins(num_quarters, num_dimes, num_nickles, num_pennies)
 
-    change = process_coins(num_quarters, num_dimes, num_nickles, num_pennies)
-
-    if change >= MENU[choice]["cost"]: # Adds change to machine if enough money for drink
-        machine_money += change
-        leftover_change = change - MENU[choice]["cost"]
-        print(f"Here is ${leftover_change} in change. ") # Print Total change given
-        make_coffee(choice)
-    else:
-        print("Sorry that's not enough money. Money refunded")    
+            if change >= MENU[choice]["cost"]: # Adds change to machine if enough money for drink
+                machine_money += change
+                leftover_change = change - MENU[choice]["cost"]
+                print(f"Here is ${leftover_change} in change. ") # Print Total change given
+                make_coffee(choice)
+            else:
+                print("Sorry that's not enough money. Money refunded")    
