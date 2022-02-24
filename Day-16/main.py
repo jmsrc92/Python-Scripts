fd76c7a31fa02c30ac110cd8f98303f09c11109e
James <jmsrc92@gmail.com>
2022-02-24 17:21:36 +0000

    upload Day 16

--- Day-16/main.py
+++ Day-16/main.py
@@ -1,18 +1,25 @@
-from turtle import Turtle, Screen
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-timmy = Turtle() # assigns timmy the class name which makes timmy an object. The paranethis at the end of class creats the object
+menu = Menu()
+tassimo = CoffeeMaker()
+money = MoneyMachine()
+tassimo_on = True
 
-my_screen = Screen()
-my_screen.setup(width=500,height=500,startx=None,starty=None)
-timmy.shape("turtle")
-timmy.color("red","green")
+while tassimo_on == True:
+    user_choice = input(f"What would you like? {menu.get_items()} : ") #Takes input from list of drinks
 
-
-timmy.forward(100)
-timmy.left(45)
-timmy.forward(25)
-timmy.left(45)
-timmy.forward(25)
-
-
-my_screen.exitonclick()
+    if user_choice == "report":
+        tassimo.report()
+        print(f"Money ${money.profit}")
+    elif user_choice == "off":
+        tassimo_on = False
+    else:
+        drink = menu.find_drink(user_choice) #finds the drink entered by user and returns object
+        if tassimo.is_resource_sufficient(drink) == None:
+            print("Not enough resources") # needs testing
+        else:    
+            money.make_payment(drink.cost)
+            tassimo.make_coffee(drink)
+      
